1d834f15f0d2a6867d791b072e7f94e1cbef7967
BR-Darkness <BRvitorgalindo@gmail.com>
2024-02-12 22:17:04 -0300

    :books: Atualização do Repositório (v3)

--- docs/app.js
+++ docs/app.js
@@ -1,4 +1,4 @@
-const config_file = "https://api.github.com/users/BR-Darkness/repos";
+const git_profile = "https://api.github.com/users/BR-Darkness/repos";
 
 /**
 * Dark Theme Function / Função de Tema Escuro
@@ -6,22 +6,22 @@ const config_file = "https://api.github.com/users/BR-Darkness/repos";
 function DarkTheme()
 {
     var Element = document.body;
-    Element.classList.toggle("Dark-Theme");
+    Element.classList.toggle("dark-theme");
 }
 
 /**
- * Função que retorna um Array com os repositórios de estudos.
- */
+* Função que retorna um Array com os repositórios de estudos.
+*/
 function Repositorios() 
 {
-    fetch("https://api.github.com/users/BR-Darkness/repos")
+    fetch(git_profile)
     .then(function (response)
     {
         if (!response.ok) 
         {
-            document.getElementById('Repositorios_List').innerHTML +=
+            document.getElementById('Repositories-List').innerHTML =
             `
-                <div class="Card">
+                <div class="card">
                     <h4 style="Text-align: center;">Erro 403:</h4>
                     <p style="Text-align: center;">A API atingiu o limite de requisições por hora, por favor volte mais tarde</p>
                     <br>
@@ -29,60 +29,34 @@ function Repositorios()
                 </div>
             `
         }
-        else
-        {
-            return response.json();
-        }
+        else return response.json();
     })
     .then(function (json)
-    {        
-        json.forEach( (Repositorio, Index) => 
+    {       
+        let data = ''; 
+        json.forEach( Repositorio => 
         {
-            if (Repositorio.homepage) 
-            {
-                document.getElementById('Repositorios_List').innerHTML +=
-                `
-                    <div class="Card">
-
-                        <h4><b>${Repositorio.name.replace(/_/gi," ")}</b></h4>
-
-                        <p><q>${Repositorio.description}</q></p>
-                        
+            data +=
+            `   
+                <div class="card">
+                    <h4>${Repositorio.name.replace(/_/gi," ")}</h4>
+                    <p><q>${Repositorio.description}</q></p>          
+                    <nav>
                         <nav>
-                            <nav>
-                                <p><b>Linguagem:</b> <i>${String(Repositorio.language).replace("null", "???")}</i></p>
-                                <p><b>Data:</b> <i>${String('####-##-##').replace(/#/g,function(m,o) {return Repositorio.created_at[o];}).split('-').reverse().join('/')}</i></p>
-                            </nav>
-                            <nav>
-                                <a href="${Repositorio.homepage}" target="_blank"><button>Ver Site</button></a>
-                                <a href="${Repositorio.html_url}" target="_blank"><button class="GithubButton">GitHub</button></a>
-                            </nav>
+                            <p><b>Linguagem:</b> ${String(Repositorio.language).replace("null", "Indefinida")}</p>
+                            <p><b>Data:</b> ${String('####-##-##').replace(/#/g,function(m,o) {return Repositorio.created_at[o];}).split('-').reverse().join('/')}</p>
                         </nav>
-                    </div>
-                `
-            }
-            else
-            {
-                document.getElementById('Repositorios_List').innerHTML +=
-                `
-                    <div class="Card">
-
-                        <h4><b>${Repositorio.name.replace(/_/gi," ")}</b></h4>
-                        
-                        <p><q>${Repositorio.description}</q></p>
-                        
                         <nav>
-                            <nav>
-                                <p><b>Linguagem:</b> <i>${String(Repositorio.language).replace("null", "???")}</i></p>
-                                <p><b>Data:</b> <i>${String('####-##-##').replace(/#/g,function(m,o) {return Repositorio.created_at[o];}).split('-').reverse().join('/')}</i></p>
-                            </nav>
-                            <nav>
-                                <a href="${Repositorio.html_url}" target="_blank"><button class="GithubButton">GitHub</button></a>
-                            </nav>
+                            <a href="${Repositorio.html_url}" target="_blank"><button class="card-github-button">GitHub</button></a>`;
+   
+                        if (Repositorio.homepage) data += `<a href="${Repositorio.homepage}" target="_blank"><button>Ver Site</button></a>`;
+                        
+                        data += `
                         </nav>
-                    </div>
-                `
-            } 
+                    </nav>
+                </div>
+            `;
+            document.getElementById('Repositories-List').innerHTML = data;
         })
     })
 }
